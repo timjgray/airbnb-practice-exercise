8daaf870ac84adc35b7facfdf2e21ebc80850400
Tim Gray <tim.gray@kellogg.northwestern.edu>
2024-01-16 13:50:30 +0000

    final SQL practice exercises

--- practice-6.sql
+++ practice-6.sql
@@ -1,5 +1,9 @@
 -- How many reviews are written per neighborhood?
 
+SELECT listings.neighborhood, COUNT(reviews.id)
+FROM reviews INNER JOIN listings ON reviews.listing_id = listings.id
+GROUP BY listings.neighborhood; 
+
 -- +------------------------+----------+
 -- | Albany Park            | 2557     |
 -- | Archer Heights         | 541      |
